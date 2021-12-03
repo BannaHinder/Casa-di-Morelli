26b89cf6696bb865145a23fdd6402aa72da5946d
Blanca Ley <blanca.ley@outlook.com>
2021-12-03 17:41:15 +0100

    Toggle menu button

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,5 @@
 // Toggle menu
-const menuList = document.querySelector( '.menu-list' ),
-      menuButton = document.querySelector('.menu-button')
-
-menuButton.addEventListener("click", () => {
-  menuList.classList.toggle("active")
-})
+function toggle() {
+  const menuList = document.querySelector('.navbar-links');
+  menuList.classList.toggle("active");
+}
