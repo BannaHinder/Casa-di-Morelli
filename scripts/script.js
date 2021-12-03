5465b5abc24fab0dbf089559105fc8cef02a1ab2
Blanca Ley <blanca.ley@outlook.com>
2021-12-03 16:55:54 +0100

    HTML and styling for top navbar

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,7 @@
-// Hi there :)
+// Toggle menu
+const menuList = document.querySelector( '.menu-list' ),
+      menuButton = document.querySelector('.menu-button')
+
+menuButton.addEventListener("click", () => {
+  menuList.classList.toggle("active")
+})
