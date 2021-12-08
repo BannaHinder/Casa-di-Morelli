694767708271aecfb036ee365bf52e6c3ff64526
fanphi <fannyp90@hotmail.com>
2021-12-08 18:20:07 +0100

    moved script tags to bottom of body

--- scripts/script.js
+++ scripts/script.js
@@ -18,13 +18,19 @@ for (let i = 0; i < menuLinks.length; i++) {
 //funktion för kontakt-formulär
 let myAddButton = document.querySelector(".skicka-btn-kontakt");
 let myPara = document.createElement("p");
+let inputSelect = document.querySelector("#choose-question");
+let nameInput = document.querySelector("#name-input");
+let emailInput = document.querySelector("#email-input");
 myPara.style.color="#FFE2B2";
+
 myAddButton.addEventListener("click",()=>{
     let myContentDiv = document.querySelector(".user-input");
-  
-  let myTextInput = document.querySelector("#text-input");
-  myPara.innerText="Tack för att du kontaktar oss! Vi återkommer snarast!";
-  myContentDiv.appendChild(myPara);
-  myContentDiv.removeChild(myTextInput);
-  
+    let myTextInput = document.querySelector("#text-input");
+    myPara.innerText="Tack för att du kontaktar oss! Vi återkommer snarast!";
+    myContentDiv.appendChild(myPara);
+    myContentDiv.removeChild(myTextInput);
+    myContentDiv.removeChild(myAddButton); 
+    myContentDiv.removeChild(inputSelect);
+    myContentDiv.removeChild(nameInput);
+    myContentDiv.removeChild(emailInput);
 });
