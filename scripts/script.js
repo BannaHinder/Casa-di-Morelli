5306df4d67c658a6d2c0740584c6b446a15120ec
BannaHinder <hanna.b.ekedahl@outlook.com>
2021-12-14 13:00:53 +0100

    changed styling recensioner moved kontakt script

--- scripts/script.js
+++ scripts/script.js
@@ -14,29 +14,3 @@ for (let i = 0; i < menuLinks.length; i++) {
   }
 }
 }
-
-//funktion för kontakt-formulär
-let myAddButton = document.querySelector(".skicka-btn-kontakt");
-let myPara = document.createElement("p");
-let inputSelect = document.querySelector("#choose-question");
-let nameInput = document.querySelector("#name-input");
-let emailInput = document.querySelector("#email-input");
-myPara.className= "contact-para";
-myPara.className="kontakt-para";
-
-myAddButton.addEventListener("click",()=>{
-    let myContentDiv = document.querySelector(".user-input");
-    let myTextInput = document.querySelector("#text-input");
-
-    if(nameInput.value===""|| emailInput.value==="" || myTextInput.value===""){
-    alert("Please fill in all input fields");
-    } 
-    else {
-    myPara.innerText="Tack för att du kontaktar oss! Vi återkommer snarast!";
-    myContentDiv.appendChild(myPara);
-    myContentDiv.removeChild(myTextInput);
-    myContentDiv.removeChild(myAddButton); 
-    myContentDiv.removeChild(inputSelect);
-    myContentDiv.removeChild(nameInput);
-    myContentDiv.removeChild(emailInput);}
-});
